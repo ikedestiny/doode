094f85fd5fa49e6f487481f83438a4deb9585893
ike destiny <ikedestiny18@gmail.com>
2025-09-16 01:39:40 +0300

    springboot struc

---
+++ backend/doode/src/main/java/com/dev/doode/DoodeApplication.java
@@ -0,0 +1,13 @@
+package com.dev.doode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoodeApplication.class, args);
+	}
+
+}
---
+++ backend/doode/src/test/java/com/dev/doode/DoodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.doode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
