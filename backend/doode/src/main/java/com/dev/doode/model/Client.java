8c01a81f40e6573c5c9d60dfd1d0867cd9d34169
destiny <ikedestiny18@gmail.com>
2025-10-30 22:12:42 +0300

    client

---
+++ backend/doode/src/main/java/com/dev/doode/model/Client.java
@@ -0,0 +1,7 @@
+package com.dev.doode.model;
+
+/**
+ * can create orders, search for kitchens by city, search for meals (TODO implement word search elastic search)
+ */
+public class Client {
+}
---
+++ backend/doode/src/test/java/com/dev/doode/controller/FoodVendorControllerTest.java
@@ -0,0 +1,28 @@
+package com.dev.doode.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FoodVendorControllerTest {
+
+    @Test
+    void getAllVendors() {
+    }
+
+    @Test
+    void getVendorById() {
+    }
+
+    @Test
+    void createVendor() {
+    }
+
+    @Test
+    void updateVendor() {
+    }
+
+    @Test
+    void deleteVendor() {
+    }
+}
